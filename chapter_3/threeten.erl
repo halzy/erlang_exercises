c50ce1d77797b15db6dea453351efbdf40a4103a
Benjamin Halsted <benjamin_halsted@yahoo.com>
2010-04-20 08:10:36 -0700

    Justify is getting closer. Just need to clean up aggregating the results, and maybe working around the floating point number precision.

--- chapter_3/threeten.erl
+++ chapter_3/threeten.erl
@@ -1,11 +1,38 @@
 -module(threeten).
--export([textProcess/2, test/1]).
+-export([textProcess/2, justifyText/2, testProcess/1, testJustify/1]).
 
 textProcess(Text, Width) ->
 	TokenText = tokenizeText(Text),
 	WordLines = makeWordLines(TokenText, Width, Width, []),
-	String = wordLinesToString(WordLines),
-	io:format("~s", [String]).
+	wordLinesToString(WordLines).
+
+justifyText(Text, Width) ->
+	TokenText = tokenizeText(Text),
+	WordLines = makeWordLines(TokenText, Width, Width, []),
+	justifyWordLines(WordLines, Width).
+
+justifyWordLines([], _Width) ->
+	ok;
+justifyWordLines([[]|WordLines], Width) ->
+	justifyWordLines(WordLines, Width);
+justifyWordLines([Line|WordLines], Width) ->
+	SpacesPerGap = spacesPerGap(Line, Width),
+	JustifiedString = lists:reverse(wordLineToString(Line, 0, "", SpacesPerGap, 0)),
+	io:format("~s", [JustifiedString]),
+	justifyWordLines(WordLines, Width).
+
+spacesPerGap([], _Width) ->
+	0;
+spacesPerGap(Line, Width) ->
+	Words = length(Line),
+	WordLength = lists:foldr(fun({_Word, Length}, Accum) -> Length + Accum end, 0, Line),
+	ExtraSpaces = Width - WordLength - Words + 1,
+	Gaps = Words-1,
+	if
+		Gaps =< 0 -> 0;
+		ExtraSpaces =< 0 -> 0;
+		ExtraSpaces > 0 -> ExtraSpaces / Gaps
+	end.
 
 wordLinesToString(WordLines) ->
 	lists:flatten(wordLinesToString(WordLines, [])).
@@ -15,17 +42,21 @@ wordLinesToString([], Strings) ->
 wordLinesToString([[]|WordLines], String) ->
 	wordLinesToString(WordLines, String);
 wordLinesToString([Line|WordLines], String1) ->
-	String2 = wordLineToString(Line, 0, String1),
+	String2 = wordLineToString(Line, 0, String1, 0, 0),
 	wordLinesToString(WordLines, String2).
 
-wordLineToString([], _WordNumber, String) ->
+wordLineToString([], _WordNumber, String, _SpacesPerGap, _GapLeftover) ->
 	NewLine = "\n",
 	[NewLine|String];
-wordLineToString([{Word, _Length}|Line], WordNumber, String) when WordNumber == 0 ->
-	wordLineToString(Line, WordNumber+1, [Word|String]);
-wordLineToString([{Word1, _Length}|Line], WordNumber, String) ->
-	Word2 = [" "|Word1],
-	wordLineToString(Line, WordNumber+1, [Word2|String]).
+wordLineToString([{Word, _Length}|Line], WordNumber, String, SpacesPerGap, _GapLeftover) when WordNumber == 0 ->
+	wordLineToString(Line, WordNumber+1, [Word|String], SpacesPerGap, SpacesPerGap);
+wordLineToString([{Word1, _Length}|Line], WordNumber1, String, SpacesPerGap, GapLeftover1) ->
+	WordNumber2 = WordNumber1+1,				% figure out the current word number
+	Spaces = erlang:trunc(GapLeftover1)		,	% drop the decimal places
+	GapLeftover2 = GapLeftover1 - Spaces + SpacesPerGap,	% adjust the remaining leftover
+	io:format("~p ~p ~p~n", [Spaces, GapLeftover1, GapLeftover2]),
+	Word2 = [string:copies(" ", Spaces+1)|Word1],
+	wordLineToString(Line, WordNumber2, [Word2|String], SpacesPerGap, GapLeftover2).
 
 % end case, out of words
 makeWordLines([], _Width, _WidthLeft, [LastLine|Lines]) ->
@@ -36,14 +67,11 @@ makeWordLines([], _Width, _WidthLeft, [LastLine|Lines]) ->
 makeWordLines([{_Word, Length}=Token|Tokens], Width, WidthLeft, [[]|Lines]) when Length =< Width ->
 	makeWordLines(Tokens, Width, WidthLeft-Length, [[Token]|Lines]);
 % line with words, and the word length is =< the length left
-makeWordLines([{_Word, Length}=Token|Tokens], Width, WidthLeft, [LastLine|Lines]) when Length =< WidthLeft ->
+makeWordLines([{_Word, Length}=Token|Tokens], Width, WidthLeft, [LastLine|Lines]) when Length < WidthLeft ->
 	makeWordLines(Tokens, Width, WidthLeft-Length-1, [[Token|LastLine]|Lines]);
 % if the line is empty, and the word length is > the width of the line
 makeWordLines([{_Word, Length}=Token|Tokens], Width, _WidthLeft, [[]|Lines]) when Length > Width ->
 	makeWordLines(Tokens, Width, 0, [[Token]|Lines]);
-% if the line is not empty and the length of the word is > the width of the line
-makeWordLines([{_Word, Length}|_Tail]=Tokens, Width, _WidthLeft, Lines) when Length > Width ->
-	makeWordLines(Tokens, Width, Width, [[]|Lines]);
 % if there is no more space on the line, reverse the tokens on the last line and push a new one on the stack
 makeWordLines(Tokens, Width, _WidthLeft, [LastLine|Lines]) ->
 	ReversedLastLine = lists:reverse(LastLine),
@@ -68,9 +96,12 @@ measureTokens(Measured, [Token|Tokens]) ->
 %% Loading Test Stubs %%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	
-test(Width) ->
+testProcess(Width) ->
 	{ok, Text} = readFileText('threeten.text'),
 	textProcess(Text, Width).
+testJustify(Width) ->
+	{ok, Text} = readFileText('threeten.text'),
+	justifyText(Text, Width).
 	
 readFileText(Filename) ->
         case file:open(Filename, read) of
